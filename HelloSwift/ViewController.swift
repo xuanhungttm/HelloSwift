95ce2a8e3e5e1fe9f0b633aeef0d575c871dbc63
Hung Doan <xuanhungttm@gmail.com>
2016-05-29 01:26:41 +0700

    Remove

---
+++ HelloSwift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by HUNG on 5/29/16.
+//  Copyright © 2016 HUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
